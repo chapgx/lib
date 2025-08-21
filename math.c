a7347649874d61edf4d95522ae306ce5447e9518
Richard Chapman Gomez <chapgx@codeh.io>
2025-08-21 08:40:16 -0400

    random number generator

--- math.c
+++ math.c
@@ -1,7 +1,8 @@
-
+#include <stddef.h>
+#include <stdint.h>
 
 // Camack's inverse square root approximation function
-float carmack_isqrt(float number) {
+float math_carmack_isqrt(float number) {
   int i;
   float x, y;
   x = number * 0.5;
@@ -12,4 +13,9 @@ float carmack_isqrt(float number) {
   y = y * (1.5 - (x * y * y));
   y = y * (1.5 - (x * y * y)); // second itearation for mor accuracy
   return y;
+}
+
+int math_rand_integer(uint32_t index) {
+  index = (index << 13) ^ index;
+  return ((index * (index * index * 15731 + 789221) + 1376312589) & 0x7fffffff);
 }
